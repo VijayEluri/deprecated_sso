55f674bdf49f435c50a5b4a491153623e0dcdf9a
Jean Boussier <jean.boussier@feedback20.com>
2010-06-24 11:21:10 +0200

    feature: C# implementation

---
+++ lib/csharp/Feedback20/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Feedback20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new Uri("https://example.users.feedback20.com/");
+            var service = new Uri("http://example.ideas.feedback20.com/");
+            var salt = "bfc9396b7c710746b19a1297e70d1716";
+
+            //it should works with only required params
+            var sso = new SSO(server, salt, service);
+            sso.FirstName = "Renaud";
+            sso.Uuid = "42";
+            sso.Expires = 1277309412;
+
+            if (sso.BuildUri().ToString() != "https://example.users.feedback20.com/cas/login?auth=sso&type=acceptor&expires=1277309412&firstname=Renaud&service=http://example.ideas.feedback20.com/&token=c34d2ee42be49635c027d1a7c39fd2f5d8411f39&uuid=42")
+            {
+                Console.Write("F");
+            }
+            else {
+                Console.Write(".");
+            }
+
+            sso.LastName = "Morane";
+            sso.AvatarUrl = new Uri("http://example.com/avatar.png");
+            sso.Email = "foo@example.com";
+            sso.Charset = "utf-8";
+            sso.Role = "user";
+
+            // it should works with all params do
+            if (sso.BuildUri().ToString() != "https://example.users.feedback20.com/cas/login?auth=sso&type=acceptor&avatar_url=http://example.com/avatar.png&charset=utf-8&email=foo@example.com&expires=1277309412&firstname=Renaud&lastname=Morane&role=user&service=http://example.ideas.feedback20.com/&token=4b44a7e193ba638700f44186de6caf3ebc270548&uuid=42")
+            {
+                Console.Write("F");
+            }
+            else
+            {
+                Console.Write(".");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lib/csharp/Feedback20/SSO.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+namespace Feedback20
+{
+    class SSO
+    {
+
+        private static string[] TOKENIZED_PARAMS = {"avatar_url", "email", "expires", "firstname", "lastname", "role", "uuid"};
+        private static string[] PARAMS = {"avatar_url", "charset", "email", "expires", "firstname", "lastname", "role", "service", "token", "uuid" };
+
+        private string salt;
+        private string server;
+        
+        private IDictionary<String, String> Parameters;
+
+        public long Timestamp { get { return (DateTime.UtcNow.Ticks - DateTime.Parse("01/01/1970 00:00:00").Ticks) / 10000000; } }
+        public Uri AvatarUrl { set { Parameters["avatar_url"] = value.ToString(); } }
+        public string Charset { set { Parameters["charset"] = value; } }
+        public string Email { set { Parameters["email"] = value; } }
+        public long ExpiresIn { set { Expires = value + Timestamp; } }
+        public long Expires { set { Parameters["expires"] = value.ToString(); } }
+        public string FirstName { set { Parameters["firstname"] = value; } }
+        public string LastName { set { Parameters["lastname"] = value; } }
+        public string Role { set { Parameters["role"] = value; } }
+        public string Uuid { set { Parameters["uuid"] = value; } }
+
+        public SSO(Uri server, String salt, Uri Service)
+        {
+            Parameters = new Dictionary<String, String>();
+            this.server = server.ToString();
+            this.salt = salt;
+            Parameters["service"] = Service.ToString();
+        }
+
+        public Uri BuildUri() 
+        {
+            Parameters["token"] = BuildToken();
+            return new Uri(server.ToString() + "cas/login?auth=sso&type=acceptor&" + TwoWayJoin("&", "=", Parameters, PARAMS));
+        }
+
+        public string BuildToken() {
+            return SHA1(TwoWayJoin(":", "-", Parameters, TOKENIZED_PARAMS) + salt);
+        }
+
+        private string TwoWayJoin(string ItemSep, string PairSep, IDictionary<string, string> Hash, string[] Keys)
+        {
+            List<string> items = new List<string>();
+            foreach (string key in Keys) {
+                if (Parameters.ContainsKey(key)) {
+                    items.Add(key + PairSep + Parameters[key]);
+                }
+            }
+            return String.Join(ItemSep, items.ToArray());           
+        }
+
+        public string SHA1(string source)
+        {
+            var UE = new ASCIIEncoding();
+            byte[] HashValue, MessageBytes = UE.GetBytes(source);
+            SHA1Managed SHhash = new SHA1Managed();
+            string strHex = "";
+            HashValue = SHhash.ComputeHash(MessageBytes);
+            foreach(byte b in HashValue) {
+                    strHex += String.Format("{0:x2}", b);
+            }
+            return strHex;
+        }
+    }
+}
