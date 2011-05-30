51aebe03d40366dc72a4730f61f9e86b82875bcf
Étienne Barrié <etienne.barrie@dimelo.com>
2011-05-30 16:13:04 +0200

    feature: java1.4: support for custom fields

--- lib/java1.4/src/com/dimelo/users/SSO.java
+++ lib/java1.4/src/com/dimelo/users/SSO.java
@@ -22,10 +22,10 @@ import java.util.Map;
 public class SSO
 {
 
-    private static String[] _PARAMS = { "avatar_url", "charset", "email",
+    private static String[] _PARAMS = { "avatar_url", "charset", "custom_field_1", "custom_field_10", "custom_field_2", "custom_field_3", "custom_field_4", "custom_field_5", "custom_field_6", "custom_field_7", "custom_field_8", "custom_field_9", "email",
             "expires", "firstname", "lastname", "role", "service", "token",
             "uuid" };
-    private static String[] _TOKENIZED_PARAMS = { "avatar_url", "email",
+    private static String[] _TOKENIZED_PARAMS = { "avatar_url", "custom_field_1", "custom_field_10", "custom_field_2", "custom_field_3", "custom_field_4", "custom_field_5", "custom_field_6", "custom_field_7", "custom_field_8", "custom_field_9", "email",
             "expires", "firstname", "lastname", "role", "uuid" };
 
     private static String join(final Collection list, final String delimiter)
